be8543e8edae20067d410a06016e7f9afb598641
Muazzam Ali <muazzamalii@hotmail.com>
2013-12-28 03:19:01 +0500

    gsy chrome extension

---
+++ gsy.js
@@ -0,0 +1,156 @@
+var GSYMemories = {
+					
+					PICTURES_URL : [
+						"pictures/1.jpg", "pictures/2.jpg",  "pictures/3.jpg", "pictures/4.jpg",
+						"pictures/5.jpg", "pictures/6.jpg",  "pictures/7.jpg", "pictures/8.jpg",
+						"pictures/9.jpg", "pictures/10.jpg", "pictures/11.jpg","pictures/12.jpg",
+						"pictures/13.jpg","pictures/14.jpg", "pictures/15.jpg","pictures/16.jpg",
+						"pictures/17.jpg","pictures/18.jpg", "pictures/19.jpg","pictures/20.jpg", 
+						"pictures/21.jpg","pictures/22.jpg", "pictures/23.jpg","pictures/24.jpg",
+						"pictures/25.jpg","pictures/26.jpg", "pictures/27.jpg","pictures/28.jpg",
+						"pictures/29.jpg","pictures/30.jpg", "pictures/31.jpg","pictures/32.jpg",
+						"pictures/33.jpg","pictures/34.jpg", "pictures/35.jpg","pictures/36.jpg",
+						"pictures/37.jpg","pictures/38.jpg", "pictures/39.jpg","pictures/40.jpg",
+						"pictures/41.jpg","pictures/42.jpg", "pictures/43.jpg","pictures/44.jpg",
+						"pictures/45.jpg","pictures/46.jpg", "pictures/47.jpg","pictures/48.jpg",
+						"pictures/49.jpg","pictures/50.jpg", "pictures/51.jpg","pictures/52.jpg",
+						"pictures/53.jpg","pictures/54.jpg", "pictures/55.jpg","pictures/56.jpg",
+						"pictures/57.jpg","pictures/58.jpg", "pictures/59.jpg","pictures/60.jpg",
+						"pictures/61.jpg","pictures/62.jpg", "pictures/63.jpg","pictures/64.jpg",
+						"pictures/65.jpg","pictures/66.jpg", "pictures/67.jpg","pictures/68.jpg",
+						"pictures/69.jpg","pictures/70.jpg", "pictures/71.jpg","pictures/72.jpg",
+						"pictures/73.jpg","pictures/74.jpg", "pictures/75.jpg","pictures/76.jpg",
+						"pictures/77.jpg","pictures/78.jpg", "pictures/79.jpg","pictures/80.jpg",
+						"pictures/81.jpg","pictures/82.jpg", "pictures/83.jpg","pictures/84.jpg",
+						"pictures/85.jpg","pictures/86.jpg", "pictures/87.jpg","pictures/88.jpg",
+						"pictures/89.jpg","pictures/90.jpg", "pictures/91.jpg","pictures/92.jpg",
+						"pictures/93.jpg","pictures/94.jpg", "pictures/95.jpg","pictures/96.jpg",
+						"pictures/97.jpg","pictures/98.jpg", "pictures/99.jpg","pictures/100.jpg",
+						"pictures/101.jpg","pictures/102.jpg", "pictures/103.jpg","pictures/104.jpg",
+						"pictures/105.jpg","pictures/106.jpg", "pictures/107.jpg","pictures/108.jpg",
+						"pictures/109.jpg","pictures/110.jpg", "pictures/111.jpg","pictures/112.jpg",
+						"pictures/113.jpg","pictures/114.jpg", "pictures/115.jpg","pictures/116.jpg",
+						"pictures/117.jpg","pictures/118.jpg", "pictures/119.jpg","pictures/120.jpg",
+						"pictures/121.jpg","pictures/122.jpg", "pictures/123.jpg","pictures/124.jpg",
+						"pictures/125.jpg","pictures/126.jpg", "pictures/127.jpg","pictures/128.jpg",
+						"pictures/129.jpg","pictures/130.jpg", "pictures/131.jpg","pictures/132.jpg",
+						"pictures/133.jpg","pictures/134.jpg", "pictures/135.jpg","pictures/136.jpg",
+						"pictures/137.jpg","pictures/138.jpg", "pictures/139.jpg","pictures/140.jpg",
+						"pictures/141.jpg","pictures/142.jpg", "pictures/143.jpg","pictures/144.jpg",
+						"pictures/145.jpg","pictures/146.jpg", "pictures/147.jpg","pictures/148.jpg",
+						"pictures/149.jpg","pictures/150.jpg", "pictures/151.jpg","pictures/152.jpg",
+						"pictures/153.jpg","pictures/154.jpg", "pictures/155.jpg","pictures/156.jpg",
+						"pictures/157.jpg","pictures/158.jpg", "pictures/159.jpg","pictures/160.jpg",
+						"pictures/161.jpg","pictures/162.jpg", "pictures/163.jpg","pictures/164.jpg",
+						"pictures/165.jpg","pictures/166.jpg", "pictures/167.jpg","pictures/168.jpg",
+						"pictures/169.jpg","pictures/170.jpg", "pictures/171.jpg","pictures/172.jpg",
+						"pictures/173.jpg","pictures/174.jpg", "pictures/175.jpg","pictures/176.jpg",
+						"pictures/177.jpg","pictures/178.jpg", "pictures/179.jpg","pictures/180.jpg",
+						"pictures/181.jpg","pictures/182.jpg", "pictures/183.jpg","pictures/184.jpg",
+						"pictures/185.jpg","pictures/186.jpg", "pictures/187.jpg","pictures/188.jpg",
+						"pictures/189.jpg","pictures/190.jpg", "pictures/191.jpg","pictures/192.jpg",
+						"pictures/193.jpg","pictures/194.jpg", "pictures/195.jpg","pictures/196.jpg",
+						"pictures/197.jpg","pictures/198.jpg", "pictures/199.jpg","pictures/200.jpg",
+						"pictures/201.jpg","pictures/202.jpg", "pictures/203.jpg","pictures/204.jpg",
+						"pictures/205.jpg","pictures/206.jpg", "pictures/207.jpg","pictures/208.jpg",
+						"pictures/209.jpg","pictures/210.jpg", "pictures/211.jpg","pictures/212.jpg",
+						"pictures/213.jpg","pictures/214.jpg", "pictures/215.jpg","pictures/216.jpg",
+
+						"pictures/217.jpg","pictures/255.jpg", "pictures/292.jpg","pictures/330.jpg",
+						"pictures/218.jpg","pictures/256.jpg", "pictures/293.jpg","pictures/331.jpg",
+						"pictures/219.jpg","pictures/257.jpg", "pictures/294.jpg","pictures/332.jpg",
+						"pictures/220.jpg","pictures/258.jpg", "pictures/295.jpg","pictures/333.jpg",
+						"pictures/221.jpg","pictures/259.jpg", "pictures/296.jpg","pictures/334.jpg",
+						"pictures/222.jpg","pictures/260.jpg", "pictures/297.jpg","pictures/335.jpg",
+						"pictures/223.jpg","pictures/261.jpg", "pictures/298.jpg","pictures/336.jpg",
+						"pictures/224.jpg","pictures/262.jpg", "pictures/299.jpg","pictures/337.jpg",
+						"pictures/225.jpg","pictures/263.jpg", "pictures/300.jpg","pictures/338.jpg",
+						"pictures/226.jpg","pictures/264.jpg", "pictures/301.jpg","pictures/339.jpg",
+						"pictures/227.jpg","pictures/265.jpg", "pictures/302.jpg","pictures/340.jpg",
+						"pictures/228.jpg","pictures/266.jpg", "pictures/303.jpg","pictures/341.jpg",
+						"pictures/229.jpg","pictures/267.jpg", "pictures/304.jpg",
+						"pictures/230.jpg","pictures/268.jpg", "pictures/305.jpg",
+						"pictures/231.jpg","pictures/269.jpg", "pictures/306.jpg",
+						"pictures/232.jpg","pictures/270.jpg", "pictures/307.jpg",
+						"pictures/233.jpg","pictures/271.jpg", "pictures/308.jpg",
+						"pictures/234.jpg","pictures/272.jpg", "pictures/309.jpg",
+						"pictures/235.jpg","pictures/273.jpg", "pictures/310.jpg",
+						"pictures/236.jpg","pictures/274.jpg", "pictures/311.jpg",
+						"pictures/237.jpg","pictures/275.jpg", "pictures/312.jpg",
+						"pictures/238.jpg","pictures/276.jpg", "pictures/313.jpg",
+						"pictures/239.jpg","pictures/277.jpg", "pictures/314.jpg",
+						"pictures/240.jpg","pictures/278.jpg", "pictures/315.jpg",
+						"pictures/241.jpg","pictures/279.jpg", "pictures/316.jpg",
+						"pictures/242.jpg","pictures/280.jpg", "pictures/317.jpg",
+						"pictures/243.jpg","pictures/281.jpg", "pictures/318.jpg",
+						"pictures/244.jpg","pictures/282.jpg", "pictures/319.jpg",
+						"pictures/245.jpg","pictures/283.jpg", "pictures/320.jpg",
+						"pictures/246.jpg","pictures/284.jpg", "pictures/321.jpg",
+						"pictures/247.jpg","pictures/285.jpg", "pictures/322.jpg",
+						"pictures/248.jpg","pictures/286.jpg", "pictures/323.jpg",
+						"pictures/249.jpg","pictures/287.jpg", "pictures/324.jpg",
+						"pictures/250.jpg","pictures/288.jpg", "pictures/325.jpg",
+						"pictures/251.jpg","pictures/289.jpg", "pictures/326.jpg",
+						"pictures/252.jpg","pictures/290.jpg", "pictures/327.jpg",
+						"pictures/253.jpg","pictures/291.jpg", "pictures/328.jpg",
+						"pictures/254.jpg","pictures/292.jpg", "pictures/329.jpg"
+					],
+					randomIndex : 0,
+										
+					setImage:function() {
+						
+						var i = this.getRandomIndex();
+
+						if(i==this.randomIndex)
+							this.setImage();
+
+						this.randomIndex = i;
+						this.changeImage(this.randomIndex);
+					},
+					
+					changeImage:function(index){
+						
+						if(index>this.PICTURES_URL.length-1)
+						{
+							index 			 = 0;
+							this.randomIndex = 0;
+						}
+						else if(index==-1)
+						{
+							this.randomIndex = this.PICTURES_URL.length-1;
+							index			 = this.PICTURES_URL.length-1;
+						}
+						var self = this;
+						$('#bg').children('img').fadeOut('slow',function(){
+							$('#bg').children('img').attr('src',self.PICTURES_URL[index]).fadeIn('slow');									
+						});
+					},
+					
+					cycle:function(){
+						var self = this;
+						
+						$('#msg_welcome').fadeOut('slow');
+						setInterval(function(){
+							self.setImage();							
+						},4500);
+					},
+				
+					getRandomIndex:function() {
+						return Math.floor(Math.random() * (this.PICTURES_URL.length - 0 + 1)) + 0;
+					},
+					
+					init:function(){
+						
+					this.changeImage(5);
+					
+					
+					var t = setInterval(function(){
+							obj.setImage();
+							obj.cycle();
+							clearInterval(t);
+						},4000);						
+					},
+					
+				};
+				var obj = Object.create(GSYMemories);
+				obj.init();
